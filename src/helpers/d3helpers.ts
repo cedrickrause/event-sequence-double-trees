49b2bf13b07c63d6b6f8b6a5fd4df46278b73446
Cedric Krause <cedric.krause@uni-due.de>
2021-08-02 14:09:16 +0200

    Removed now unused helper

--- src/helpers/d3helpers.ts
+++ src/helpers/d3helpers.ts
@@ -1,10 +1,3 @@
 import * as d3 from 'd3';
 
 export type SvgSelection = d3.Selection<SVGSVGElement, unknown, null, unknown>;
-
-export interface HierarchyDatum {
-  type: string;
-  value: number;
-  children: Array<HierarchyDatum>;
-  highlight?: boolean;
-}
